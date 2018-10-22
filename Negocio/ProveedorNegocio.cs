1933bd8e9abf72593af977d06af1fab007400cc8
Daniela Rubinstein <nanurubinstein@hotmail.com>
2018-10-21 22:38:40 -0300

    Listar

--- Negocio/ProveedorNegocio.cs
+++ Negocio/ProveedorNegocio.cs
@@ -25,7 +25,6 @@ namespace Negocio
                 {
                     aux = new Proveedor();
                     aux.IdContacto = (int)conexion.Lector["IdContacto"];
-
                 }
                 return lista;
             }
